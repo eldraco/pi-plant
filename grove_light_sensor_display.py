f1f16f4e40e22301fe754551c0510cf697c0785c
eldraco <eldraco@gmail.com>
2021-03-20 16:51:24 +0000

    Plant 1 humidity sensor

--- grove_light_sensor_display.py
+++ grove_light_sensor_display.py
@@ -43,37 +43,43 @@ from grove_rgb_lcd import *
 from math import isnan
 import Adafruit_IO
 from dotenv import load_dotenv
+import os
 
 print('Starting')
 
 
 # Connect to Adafruit mqtt server
 load_dotenv('.env')
-client.run(os.getenv('ADAFRUITUSER'))
-client.run(os.getenv('ADAFRUITKEY'))
-client.run(os.getenv('ADAFRUITTOPIC'))
+ADAFRUITUSER = os.getenv('ADAFRUITUSER')
+ADAFRUITKEY = os.getenv('ADAFRUITKEY')
 
 AIOclient = Adafruit_IO.MQTTClient(ADAFRUITUSER, ADAFRUITKEY)
 AIOclient.connect()
 
+# Humidity-Temp sensor setup
 dht_sensor_port = 7 # connect the DHt sensor to port 7
 dht_sensor_type = 0 # use 0 for the blue-colored sensor and 1 for the white-colored sensor
 
 # Connect the Grove Light Sensor to analog port A0
-# SIG,NC,VCC,GND
 light_sensor = 0
 
+# Connect the Grove Light Sensor to analog port A1
 sound_sensor = 1
 
+# Connect the Grove Light Sensor to analog port A2
+humplant1_sensor = 2
+
 # Connect the LED to digital port D4
-# SIG,NC,VCC,GND
 led = 4
 
 # Turn on LED once sensor exceeds threshold resistance
 threshold = 10
 
-#Start and initialize the OLED
+# Start and initialize the display
 setRGB(0,255,0)
+
+# Read interval
+read_interval = 15
 
 
 grovepi.pinMode(light_sensor,"INPUT")
@@ -95,6 +101,11 @@ while True:
         [ temp,hum ] = dht(dht_sensor_port,dht_sensor_type)
         AIOclient.publish('Temperature', temp)
         AIOclient.publish('Humidity', hum)
+
+        # Get humidity of plant 1
+        humplant1_value = grovepi.analogRead(humplant1_sensor)
+        AIOclient.publish('Plant1_hum', humplant1_value)
+        print(f'Hum plant1 = {humplant1_value}')
 
         # check if we have nans
         # if so, then raise a type error exception
@@ -120,7 +131,7 @@ while True:
 
         #print("sensor_value = %d resistance = %.2f" %(sensor_value,  resistance))
 
-        time.sleep(10)
+        time.sleep(read_interval)
 
     except (IOError, TypeError) as e:
         print(str(e))
